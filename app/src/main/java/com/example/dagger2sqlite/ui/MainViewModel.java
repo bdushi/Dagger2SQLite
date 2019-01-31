255cd8f3b5c6408f77e0858915c08a5b7a277769
bdushi <brunodushi@gmail.com>
2019-01-31 14:12:22 +0100

    Working with Dagger impl for MVVM

--- app/src/main/java/com/example/dagger2sqlite/ui/MainViewModel.java
+++ app/src/main/java/com/example/dagger2sqlite/ui/MainViewModel.java
@@ -5,12 +5,14 @@ import com.example.dagger2sqlite.model.User;
 
 import java.util.List;
 
+import javax.inject.Inject;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
 
     private final UserRepository userRepository;
 
+    @Inject
     public MainViewModel(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
