d66c6076c57dfc91a932d69d4e7fe2428d7baa24
bdushi <brunodushi@gmail.com>
2019-01-31 15:38:58 +0100

    Clean Code

--- app/src/main/java/com/example/dagger2sqlite/di/AppModule.java
+++ app/src/main/java/com/example/dagger2sqlite/di/AppModule.java
@@ -11,39 +11,4 @@ import dagger.Module;
 public abstract class AppModule {
     @Binds
     abstract Context bindContext(Application application);
-
-    /*@Singleton
-    @Provides
-    fun provideGithubService(): GithubService {
-        return Retrofit.Builder()
-                .baseUrl("https://api.github.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(LiveDataCallAdapterFactory())
-                .build()
-                .create(GithubService::class.java)
-    }*/
-
-    /*private final MyApplication myApplication;
-
-    public AppModule(MyApplication myApplication){
-        this.myApplication = myApplication;
-    }
-
-    @Provides
-    @Singleton
-    MyApplication providesApplicationContext() {
-        return myApplication;
-    }
-
-    @Provides
-    @Singleton
-    SharedPreferences providesSharedPreferences(Context context) {
-        return context.getSharedPreferences("My_Pref", Context.MODE_PRIVATE);
-    }
-
-    @Provides
-    @Singleton
-    SQLiteDatabase providesDatabaseHelper(Context context) {
-        return new LocalDatabaseHelper(context).getWritableDatabase();
-    }*/
 }
