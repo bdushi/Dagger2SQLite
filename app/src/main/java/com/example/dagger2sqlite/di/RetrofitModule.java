e9c2376d9dbf4053f38d6f64cdb8f6e52446e5a6
bdushi <brunodushi@gmail.com>
2019-03-28 12:03:40 +0100

    Add BasURl provider

--- app/src/main/java/com/example/dagger2sqlite/di/RetrofitModule.java
+++ app/src/main/java/com/example/dagger2sqlite/di/RetrofitModule.java
@@ -3,6 +3,8 @@ package com.example.dagger2sqlite.di;
 import com.example.dagger2sqlite.model.User;
 import com.example.dagger2sqlite.services.UserService;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
@@ -31,17 +33,24 @@ public abstract class RetrofitModule {
         }
     })*/
 
+    //"http://5.189.158.56:8080/ms/api"
     @Provides
     @Singleton
-    public static Retrofit providesRetrofit(final User user) {
+    public static String providesBaseUrl() {
+        return "http://5.189.158.56:8080/ms/api";
+    }
+    @Provides
+    @Singleton
+    public static Retrofit providesRetrofit(final User user, String baseUrl) {
         return new Retrofit.Builder()
-                .baseUrl(String.format("%s/%s/", "http://5.189.158.56:8080/ms/api", "v1"))
+                .baseUrl(String.format("%s/%s/", baseUrl, "v1"))
                 .addConverterFactory(MoshiConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .client(new OkHttpClient.Builder()
                         .addInterceptor(new Interceptor() {
+                            @NotNull
                             @Override
-                            public Response intercept(Interceptor.Chain chain) throws IOException {
+                            public Response intercept(@NotNull Interceptor.Chain chain) throws IOException {
                                 Request request = chain.request();
                                 Request authenticatedRequest = request.newBuilder().header("Authorization", Credentials.basic(user.getUsername(), user.getUsername())).build();
                                 return chain.proceed(authenticatedRequest);
